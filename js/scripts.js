f6df9705c557aeba394c79ed8a49ccfa62cffd95
Sam Martinez <sammartinez19@gmail.com>
2015-09-18 10:47:14 -0700

    add second test and pass

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 // JavaScript goes here
-function Pizza(quanity, toppings, pizzaSize) {
+function Pizza(quanity, pizzaSize, toppings) {
     this.quanity = quanity;
     this.toppings = toppings;
     this.pizzaSize = pizzaSize;
+}
+
+Pizza.prototype.fullOrder = function() {
+    return this.quanity + " " + this.pizzaSize + " " + this.toppings + " pizza(s)";
+}
+
+function Topping(toppingItem, toppingCost) {
+    this.toppingItem = toppingItem;
+    this.toppingCost = toppingCost;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,14 @@
 describe('Pizza', function() {
     it("creates the pizza the customer ordered.", function() {
-        var testPizza = new Pizza(1, "pepperoni", "large");
+        var testPizza = new Pizza(1, "large", "pepperoni");
         expect(testPizza.quanity).to.equal(1);
-        expect(testPizza.toppings).to.equal("pepperoni");
         expect(testPizza.pizzaSize).to.equal("large");
+        expect(testPizza.toppings).to.equal("pepperoni");
     });
 
+    it("adds the entire order together", function() {
+        var testPizza = new Pizza(2, "medium", "cheese");
+        expect(testPizza.fullOrder()).to.equal("2 medium cheese pizza(s)");
+    });
 
 });

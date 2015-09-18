8ea28adeefbab76fc3f8813bca2d71f91531a601
Sam Martinez <sammartinez19@gmail.com>
2015-09-18 14:28:56 -0700

    having tests fail. restart with basics and use prototype with the pizza totalcost

--- js/scripts.js
+++ js/scripts.js
@@ -9,60 +9,7 @@ Pizza.prototype.fullOrder = function() {
     return this.quanity + " " + this.pizzaSize + " " + this.toppings + " pizza(s)";
 }
 
-function totalCost(order) {
-    var orderCost = 0;
-
-    switch(order.pizzaSize) {
-        case "small":
-        orderCost = 5 * order.quanity
-        break;
-
-        case "medium":
-        orderCost = 7 * order.quanity
-        break;
-
-        case "large":
-        orderCost = 10 * order.quanity
-        break;
-    }
-
-    if(this.toppings !== "cheese") {
-        this.toppings.forEach(function(topping) {
-            orderCost += 1;
-        )};
-    }
-
-    return orderCost;
-}
 
 $(document).ready(function() {
-    event.preventDefault();
 
-    $("#add-toppings").click(function(event) {
-        event.preventDefault();
-    $("#new-topping").append("<div class='form-group new-topping'>" +
-        "<select id='new-top'>" +
-        "<option value='cheese'>Cheese</option>" +
-        "<option value='pepperoni'>Pepperoni</option>" +
-        "<option value='sausage'>Sausage</option>" +
-        "<option value='bacon'>Bacon</option>" +
-        "<option value='pineapple'>Pineapple</option>" +
-        "</select>" +
-        "</div>")
-    });
-
-    $("form#new-order").submit(function(event) {
-        event.preventDefault();
-        var quanity = $("input#quanity").val();
-        var pizzaSize = $("select#pizzaSize").val();
-        var toppings = [];
-
-        $(".new-topping").each(function() {
-            toppings.push("select#new-top").val();
-        });
-
-        var newPizza = new Pizza(quanity, pizzaSize, toppings);
-
-        $("#total-cost").text(newPizza.totalCost(order));
-    });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -17,34 +17,9 @@ describe('Pizza', function() {
     });
 });
 
-describe('totalCost', function() {
-    it("charges $5 for a small pizza", function() {
+describe('Pizza.totalCost', function() {
+    it("charges the customer $5 for a small pizza", function() {
         var testPizza = new Pizza(1, "small");
-        expect(totalCost(testPizza)).to.equal(5);
-    });
-
-    it("charges $7 for a medium pizza", function() {
-        var testPizza = new Pizza(1, "medium");
-        expect(totalCost(testPizza)).to.equal(7);
-    });
-
-    it("charges $10 for a large pizza", function() {
-        var testPizza = new Pizza(1, "large");
-        expect(totalCost(testPizza)).to.equal(10);
-    });
-
-    it("charges $10 for two small pizzas", function() {
-        var testPizza = new Pizza(2, "small");
-        expect(totalCost(testPizza)).to.equal(10);
-    });
-
-    it("charges $14 for two medium pizzas", function() {
-        var testPizza = new Pizza(2, "medium");
-        expect(totalCost(testPizza)).to.equal(14);
-    });
-
-    it("charges $20 for two large pizzas", function() {
-        var testPizza = new Pizza(2, "large");
-        expect(totalCost(testPizza)).to.equal(20);
+        expect(testPizza.totalCost()).to.equal(5);
     });
 });

ab37adba4da5c4335abbe03494db15d0a2502c25
Sam Martinez <sammartinez19@gmail.com>
2015-09-18 15:15:41 -0700

    add comments to all pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,15 @@
-// JavaScript goes here
+//Base function on the project which takes the three arugments quanity, pizzaSize, and toppings
 function Pizza(quanity, pizzaSize, toppings) {
     this.quanity = quanity;
     this.toppings = toppings;
     this.pizzaSize = pizzaSize;
 }
 
-Pizza.prototype.fullOrder = function() {
-    return this.quanity + " " + this.pizzaSize + " " + this.toppings + " pizza(s)";
-}
-
+//Used to calculate cost of the pizza based on default cost and quanity
 Pizza.prototype.totalCost = function() {
     var costOfOrder = 0;
 
+    //Depending on what the user selects, it adds the cost of the pizza times the quanity
     if(this.pizzaSize === "small") {
         costOfOrder = 5 * this.quanity;
     } else if(this.pizzaSize === "medium") {
@@ -22,6 +20,7 @@ Pizza.prototype.totalCost = function() {
         costOfOrder = 15 * this.quanity;
     }
 
+    //Adds a $1 for each new topping that is added to the order.
     if (this.toppings !== undefined) {
     this.toppings.forEach(function(topping) {
       costOfOrder += 1;
@@ -30,9 +29,13 @@ Pizza.prototype.totalCost = function() {
     return costOfOrder;
 }
 
+//JQuery starts here
 $(document).ready(function() {
   $("#add-toppings").click(function(event) {
     event.preventDefault();
+
+    /* When the button "Add toppings" is pressed, this dropdown menu will display
+    all of the information within the selections listed below */
     $("#new-topping").append(
       '<div class="form-group new-topping">' +
         '<select id="toppings">' +
@@ -44,6 +47,8 @@ $(document).ready(function() {
       '</div>')
   });
 
+  /* When the submit button is pressed, all of the information from the form calculates
+    the cost of the order and displays it within a span at the bottom of the page */
   $("form#new-order").submit(function(event) {
     event.preventDefault();
 
@@ -51,6 +56,7 @@ $(document).ready(function() {
     var pizzaSize = $("select#sizeOfPizza").val();
     var toppings = [];
 
+    //Takes the toppings selected by the user and pushes it into the blank toppings array.
     $(".new-topping").each(function() {
       toppings.push($("select#toppings").val());
     });
@@ -58,5 +64,6 @@ $(document).ready(function() {
     var newPizzaOrder = new Pizza(quantity, pizzaSize, toppings);
 
     $("#order-price").text(newPizzaOrder.totalCost());
+    $("#results").show();
   });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -5,17 +5,12 @@ describe('Pizza', function() {
         expect(testPizza.pizzaSize).to.equal("large");
         expect(testPizza.toppings).to.equal("pepperoni");
     });
-
-    it("adds the entire order together", function() {
-        var testPizza = new Pizza(2, "medium", "cheese");
-        expect(testPizza.fullOrder()).to.equal("2 medium cheese pizza(s)");
-    });
-
     it("adds multiple toppings to a single pizza", function() {
         var testPizza = new Pizza(1, "small", ["sausage", "pepperoni", "pineapple"]);
         expect(testPizza.toppings).to.eql(["sausage", "pepperoni", "pineapple"]);
     });
 
+//For the following tests below, this is where I use the Pizza.prototype.totalCost function.
     it("charges the customer $5 for a small pizza", function() {
         var testPizza = new Pizza(1, "small");
         expect(testPizza.totalCost()).to.equal(5);

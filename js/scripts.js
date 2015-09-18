483e9d6b98f44dfab4f7a1f1f797656d44fd8bf2
Sam Martinez <sammartinez19@gmail.com>
2015-09-18 11:59:14 -0700

    change totalCost to a switch statement

--- js/scripts.js
+++ js/scripts.js
@@ -12,16 +12,20 @@ Pizza.prototype.fullOrder = function() {
 function totalCost(order) {
     var orderCost = 0;
 
-    if(order.pizzaSize === "small") {
-        orderCost = 5 * order.quanity;
-    } else if(order.pizzaSize === "medium") {
-        orderCost = 7 * order.quanity;
-    } else if (order.pizzaSize === "large") {
-        orderCost = 10 * order.quanity;
-    }
+    switch(order.pizzaSize) {
+        case "small":
+        orderCost = 5 * order.quanity
+        break;
 
-    // if((order.pizzaSize === "small") && (order.pizzaSize === "medium") && (order.pizzaSize === "large")) {
-    //     orderCost = 22;
-    // }
+        case "medium":
+        orderCost = 7 * order.quanity
+        break;
+
+        case "large":
+        orderCost = 10 * order.quanity
+        break;
+    }
     return orderCost;
 }
+
+$(document).ready

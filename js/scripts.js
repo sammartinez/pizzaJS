c636a14c002115f1692476363120e7faa3087a4f
Sam Martinez <sammartinez19@gmail.com>
2015-09-18 10:29:20 -0700

    add first test and pass

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
 // JavaScript goes here
+function Pizza(quanity, toppings, pizzaSize) {
+    this.quanity = quanity;
+    this.toppings = toppings;
+    this.pizzaSize = pizzaSize;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,1 +1,10 @@
-// JavaScript Specs go here
+describe('Pizza', function() {
+    it("creates the pizza the customer ordered.", function() {
+        var testPizza = new Pizza(1, "pepperoni", "large");
+        expect(testPizza.quanity).to.equal(1);
+        expect(testPizza.toppings).to.equal("pepperoni");
+        expect(testPizza.pizzaSize).to.equal("large");
+    });
+
+
+});
